3c7645a38beca75aa3829002e2ed299b967db6fc
jobinkvarghese <jobinkvarghese7@gmail.com>
2025-02-07 18:33:29 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,50 @@
+//Callback Function
+// function display(sum){
+//      console.log(sum);
+// }
+// function add(x,y,callbackfn){
+//     let sum=x+y;
+//     callbackfn(sum);
+
+// }
+// add(5,5,display);
+// const person={
+//     name:"bill gates",
+//     age:50,
+//     isrich:true
+// };
+
+// let {name,age,isrich}=person;
+// console.log(name,age,isrich);
+
+// const literal={
+//     name,
+//     age,
+//     isrich
+// };
+// console.log(literal);
+
+
+// let div=document.getElementById("js");
+// console.log(div);
+// let cls=document.getElementsByClassName("p")
+// console.log(cls);
+// let tag=document.getElementsByTagName("span")
+// console.log(tag);
+// let p=document.getElementsByName("java")
+// console.log(p);
+// let q=document.querySelector("#span")
+// console.log(q);
+// let s=document.querySelectorAll(".p")
+// console.log(s);
+
+
+let div=document.querySelector("#js")
+console.log(div.style.cssText+="color:white;font-size:20px;");
+
+
+
+// console.log(pro);
+// div.addEventListener("mouseover",()=>{  console.log(this)
+// }
+// )
